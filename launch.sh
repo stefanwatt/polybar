b9debbe6e1fcb1f4910e1a20834f590beaf9f1ea
stefan watt <stefan.watt@gmail.com>
2022-03-08 01:03:14 +0100

    updated path

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 
 #boot a polybar for each monitor
-cd /home/stefan/polybar
+cd /home/stefan/.config/polybar
 polybar -m | sed s/:.*// | tac | while read -r monitor
 do 
   monitor=$monitor polybar -c config.ini main &> /dev/null &
